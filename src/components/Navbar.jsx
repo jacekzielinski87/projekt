c2e44b1262b241a82977530cdeecab823f907191
Jacek Zielinski <jacekzielinski0787@gmail.com>
2023-08-28 14:41:35 +0200

    Start Project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav className="navigation">
+            <li className="list-items"><a href="/">Strona główna</a></li>
+            <li className="list-items"><a href="/">Strona główna</a></li>
+            <li className="list-items"><a href="/">Strona główna</a></li>
+            <li className="list-items"><a href="/">Strona główna</a></li>
+        </nav>
+    );
+};
+
+export default Navbar;
