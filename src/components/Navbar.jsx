5fefd0728731288ed6cd23b6af31a9db411faf77
Jacek Zielinski <jacekzielinski0787@gmail.com>
2023-09-01 23:46:16 +0200

    Navbar Section added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,13 @@ import React from "react";
 const Navbar = () => {
     return (
         <nav className="navigation">
-            <li className="list-items"><a href="/">Strona główna</a></li>
-            <li className="list-items"><a href="/">Strona główna</a></li>
-            <li className="list-items"><a href="/">Strona główna</a></li>
-            <li className="list-items"><a href="/">Strona główna</a></li>
+            <ul className="nav-list">
+                <li className="list-items"><a className="link" href="/">Strona główna</a></li>
+                <li className="list-items"><a className="link" href="/">Aktualności</a></li>
+                <li className="list-items"><a className="link" href="/">Recenzje</a></li>
+                <li className="list-items"><a className="link" href="/">Filmy</a></li>
+                <li className="list-items"><a className="link" href="/">Seriale</a></li>
+            </ul>
         </nav>
     );
 };
