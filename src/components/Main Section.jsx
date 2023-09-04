cbcf61125417814e5dc8efcb6fb9f7077790eae3
Jacek Zielinski <jacekzielinski0787@gmail.com>
2023-09-04 13:04:29 +0200

    Add Main Section

---
+++ src/components/Main Section.jsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+
+
+const Main = () => {
+    return (
+        <div className="Grid-one">First Grid</div>
+        <div className="Grid-two">First Two</div>
+        <div className="Grid-three">First Three</div>
+        <div className="Grid-four">First Four</div>
+    )
+}
+    
+
+export default Main;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,20 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import Logo from "../assets/gamin small.png";
+
 
 const Navbar = () => {
     return (
         <nav className="navigation">
+            <img className="logo" src={Logo}></img>
+                <p className="description">Miejsce dla każdego gracza</p>
             <ul className="nav-list">
                 <li className="list-items"><a className="link" href="/">Strona główna</a></li>
                 <li className="list-items"><a className="link" href="/">Aktualności</a></li>
                 <li className="list-items"><a className="link" href="/">Recenzje</a></li>
-                <li className="list-items"><a className="link" href="/">Filmy</a></li>
-                <li className="list-items"><a className="link" href="/">Seriale</a></li>
+                <li className="list-items"><a className="link" href="/">Retro</a></li>
+                <li className="list-items"><a className="link" href="/">Książki</a></li>
+                <li className="list-items"><a className="link" href="/">Filmy i seriale</a></li>
             </ul>
         </nav>
     );
