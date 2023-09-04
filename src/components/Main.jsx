3c209eafef98d4def3c18f5c80e808226bb8f0f5
Jacek Zielinski <jacekzielinski0787@gmail.com>
2023-09-04 13:27:45 +0200

    Css Grid secton

---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+
+
+const Main = () => {
+    return (
+        <div className="Main">
+            <div className="Grid">First Grid</div>
+            <div className="Grid">First Two</div>
+            <div className="Grid">First Three</div>
+            <div className="Grid">First Four</div>
+        </div>
+    )
+}
+    
+
+export default Main;
