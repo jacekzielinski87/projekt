9c35b9f3f8a7ab82d0bdda638ed648ec151b76dc
Jacek Zielinski <jacekzielinski0787@gmail.com>
2023-09-08 15:01:25 +0200

    Added new grid elements, planing posts position

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,9 +6,22 @@ import React from "react";
 const Main = () => {
     return (
         <div className="Main">
-            <div className="Grid-item-1">Sidebar Content 1</div>
-            <div className="Grid-item-2">Center content</div>
-            <div className="Grid-item-3">Sidebar content 2</div>
+            <div className="Grid-item-1">Content 1</div>
+            <div className="Grid-item-2">Content 2</div>
+            <div className="Grid-item-3">Content 3</div>
+            <div className="Grid-item-3">Content 4</div>
+            <div className="Grid-item-1">Content 1</div>
+            <div className="Grid-item-2">Content 2</div>
+            <div className="Grid-item-3">Content 3</div>
+            <div className="Grid-item-3">Content 4</div>
+            <div className="Grid-item-1">Content 1</div>
+            <div className="Grid-item-2">Content 2</div>
+            <div className="Grid-item-3">Content 3</div>
+            <div className="Grid-item-3">Content 4</div>
+            <div className="Grid-item-1">Content 1</div>
+            <div className="Grid-item-2">Content 2</div>
+            <div className="Grid-item-3">Content 3</div>
+            <div className="Grid-item-3">Content 4</div>
         </div>
     )
 }
